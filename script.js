353feb8f48367fe0b865c6b7f8a00fd24d95de14
Angelica <gelimia2@gmail.com>
2021-12-12 01:24:45 +0000

    Theme changes

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#skate').addEventListener('click', function() {
+        document.location = "skateboard.html";
+    });
+    document.querySelector('#dj').addEventListener('click', function() {
+        document.location = "dj.html";
+    });
+    document.querySelector('#rubik').addEventListener('click', function() {
+        document.location = "rubik.html";
+    });
+});
